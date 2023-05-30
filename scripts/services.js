ebea5d48232f51fb9a73d7e616a2347674cb3a6e
Vadym_Benedict <o.oo.o@zoho.com>
2023-05-31 01:05:31 +0300

    our services completed and purchase section start

--- scripts/services.js
+++ scripts/services.js
@@ -1,23 +1,16 @@
-const bookmark = document.querySelectorAll(".bookmarkItem");
-let bookmarksField = document.getElementsByClassName("bookmarks");
-const contentContainer = document.getElementsByClassName("text-content");
-const contentText = document.querySelectorAll(".content-item");
-
-function remActive(obj) {
-    obj.forEach((tab) => tab.classList.remove("active"));
-}
-
 document.body.addEventListener("click", function(event) {
     event.preventDefault()
     if  (event.target.closest(".bookmark-item")) {
-        // remActive(bookmark);
-        const actBookmark = bookmarksField.querySelector(".active");
-        actBookmark.classList.remove("active");
-        // remActive(contentText);
+        const  actTab = document.body.querySelector(".active");
+        actTab.classList.remove("active");
 
-        // event.target.classList.add("active");
-        // const contentId = event.target.dataset.mark;
-        // const content = document.getElementById(contentId);
-        // content.classList.add("active");
+        const actArticle = document.querySelector(".text-content");
+        const activeText = actArticle.querySelector(".active");
+        activeText.classList.remove("active");
+
+        event.target.classList.add("active");
+        const mark = event.target.dataset.mark;
+        const content = document.getElementById(mark);
+        content.classList.add("active");
     }
 })
